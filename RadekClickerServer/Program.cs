9ad735fb849da74ed6ff21a8b485f9a377fed2f9
chamik <jakub.hampl@email.cz>
2022-12-20 15:56:48 +0100

    Moved secrets to appsettings

--- RadekClickerServer/Program.cs
+++ RadekClickerServer/Program.cs
@@ -3,27 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RadekClickerServer;
 
-// i know ;-;
-const string ADMIN_SECRET = "5976d3b4f49f6ae9d20edef64dfbbcee34452d3e";
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PlayerDb>(c => c.UseSqlite("Data Source=db/radek.db"));
 
 var app = builder.Build();
 
 // random hash idk
-var hashids = new Hashids("78b28ded1c9d7afd170edb1", 6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+var hashids = new Hashids(app.Configuration.GetValue("hashSalt", ""), 6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
 
 app.MapGet("/leaderboard", ([FromServices]PlayerDb db) =>
     db.Players.OrderByDescending(x => x.Radeks));
 
-app.MapGet("/gethash/{secret}/{id:int}", (string secret, int id) => 
-    secret != ADMIN_SECRET ? Results.Unauthorized() : Results.Ok(hashids.Encode(id)));
+app.MapGet("/gethash/{secret}/{id:int}", ([FromServices]IConfiguration cf, string secret, int id) => 
+    secret != cf.GetValue("adminSecret", "") ? Results.Unauthorized() : Results.Ok(hashids.Encode(id)));
 
-app.MapPut("/newplayer/{secret}/{name}", async ([FromServices]PlayerDb db, string secret, string name) =>
+app.MapPut("/newplayer/{secret}/{name}", async ([FromServices]PlayerDb db, [FromServices]IConfiguration cf, string secret, string name) =>
 {
     // you don't have to comment on this :)
-    if (secret != ADMIN_SECRET)
+    if (secret != cf.GetValue("adminSecret", ""))
         return Results.Unauthorized();
 
     if (db.Players.Select(x => x.DisplayName).Contains(name))
